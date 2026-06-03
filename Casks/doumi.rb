2ed6740584e1445a9f55b10164c5f512da22c3b7
andreas nicholas <xmacbookm5@andreass-MacBook-Pro.local>
2026-06-03 06:39:49 -0700

    Set SHA256 for Doumi v0.1.1 DMG

--- Casks/doumi.rb
+++ Casks/doumi.rb
@@ -1,7 +1,6 @@
 cask "doumi" do
   version "0.1.1"
-  sha256 arm:   "<ARM_SHA256>",
-         intel: "<INTEL_SHA256>"
+  sha256 "5d983097e1e62bdd99f1b5a32aad22949e009e2368524745253858ee8ffadb5a"
 
   url "https://github.com/xirtus/doumi/releases/download/v#{version}/Doumi-#{version}.dmg"
   name "Doumi"
